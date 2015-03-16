68cf48f7f493811e396bf89274e7a2609ee47bb3
Jörg Battermann <jb@joergbattermann.com>
2015-03-16 20:11:28 +0100

    Initial commit

---
+++ jB.TfsToolkit/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+
+using System;
+
+namespace Joerg.Battermann.TfsToolkit
+{
+    static class GuidList
+    {
+        public const string guidTfsToolkitPkgString = "becd428b-780f-4008-87f3-070d1dbe1038";
+        public const string guidTfsToolkitCmdSetString = "a6ce3d8f-b2ac-4131-98a8-b091f8871c92";
+
+        public static readonly Guid guidTfsToolkitCmdSet = new Guid(guidTfsToolkitCmdSetString);
+    };
+}
